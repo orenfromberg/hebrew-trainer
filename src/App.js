47c35c618302d65c6e9c40a7612f437af60188a8
Oren Fromberg <oren@adhocteam.us>
2019-10-08 00:24:03 -0400

    Adding questions

--- src/App.js
+++ src/App.js
@@ -2,17 +2,114 @@ import React, { useState } from "react";
 import "./App.css";
 import Question from "./Question";
 
+const questions = [
+  {
+    prompt: "א",
+    regex: /[alef|aleph]/i
+  },
+  {
+    prompt: "בּ",
+    regex: /[bet|beth]/i
+  },
+  {
+    prompt: "ב",
+    regex: /vet/i
+  },
+  {
+    prompt: "גּ",
+    regex: /gimel/i
+  },
+  {
+    prompt: "ג",
+    regex: /ghimel/i
+  },
+  {
+    prompt: "דּ",
+    regex: /daleth/i
+  },
+  {
+    prompt: "ד",
+    regex: /dhaleth/i
+  },
+  {
+    prompt: "ה",
+    regex: /hey/i
+  },
+  {
+    prompt: "ו",
+    regex: /vav/i
+  },
+  {
+    prompt: "ז",
+    regex: /zayin/i
+  },
+  {
+    prompt: "ח",
+    regex: /chet/i
+  },
+  {
+    prompt: "ט",
+    regex: /tet/i
+  },
+  {
+    prompt: "י",
+    regex: /yud/i
+  },
+  {
+    prompt: "כּ",
+    regex: /kaph/i
+  },
+  {
+    prompt: "כ",
+    regex: /chaf/i
+  },
+  {
+    prompt: "ך",
+    regex: /chaf sofit/i
+  },
+  {
+    prompt: "ל",
+    regex: /lamedh/i
+  },
+  {
+    prompt: "מ",
+    regex: /mem/i
+  },
+  {
+    prompt: "ם",
+    regex: /final mem/i
+  },
+  {
+    prompt: "נ",
+    regex: /nun/i
+  },
+  {
+    prompt: "ן",
+    regex: /final nun/i
+  },
+  {
+    prompt: "ס",
+    regex: /samech/i
+  }
+];
+
 function App() {
-  const [prompt, setPrompt] = useState("א");
-  const [regex, setRegex] = useState(/alef/i);
+  const [count, setCount] = useState(0);
+  const [prompt, setPrompt] = useState(questions[0].prompt);
+  const [regex, setRegex] = useState(questions[0].regex);
+
   const [result, setResult] = useState("unanswered");
 
   const test = answer => {
     setResult(regex.test(answer) ? "correct" : "incorrect");
     setTimeout(() => {
       setResult("unanswered");
-      setPrompt("ב");
-      setRegex(/bet/i);
+
+      let i = (count + 1) % questions.length;
+
+      setPrompt(questions[i].prompt);
+      setRegex(questions[i].regex);
+      setCount(i);
     }, 1000);
   };
 
